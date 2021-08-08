9c4e50124b4892c21d804fe50df6b49e8aa10c32
Alexandra Volentir <67002616+AlexandraVolentir@users.noreply.github.com>
2021-08-08 08:10:38 +0300

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,25 +1,47 @@
 # This would be a pretty nice hangman game
 
 def updated_current_word(initial_word, current_word, letter):
-    if current_word.isspace():
+    if '*' in current_word:
         for i in range(len(initial_word)):
             if initial_word[i] ==  letter:
                 current_word[i] = letter
     return current_word
 
-container = input("Enter your word/phrase: ").strip()
-print("so \"" + container + "\" would be the phrase to guess for the player")
+content = ["Animals", "Home utilities", "Clothes", "Food", "Jobs",
+           "Body", "Computer", "Personal", "Sports", "Kitchen", 
+           "Bathroom", "Nature", "Stationery", "Music", "Transport", 
+           "Healthcare", "Buildings", "Places/Landscapes", 
+           "Numbers", "Countries",  "Garden", "Things", "Pool"
+            "Colors"]
 
 done = False
-length = len(container)
+while not(done):
+    cathegory = input("-------------Please choose your cathegory: -------------------\n" + " ".join(content) + "\nYour cathegory is: ").capitalize();
+    done = cathegory.capitalize() in content
+    if not(done):
+        print("You entered an unexisting cathegory. try again: ")
+str_container = input("Enter your word/phrase: ").strip().lower()
+container = list(str_container)
+print("so \"" + str_container + "\" would be the phrase to guess\n-------------------------\n")
+
+done = False
+length = len(str_container)
 k = 0
-cur_word = " "*length;
+cur_word = list("*"*length);
 
-while(k != 9): # 9 parts of the body which need to be drawn unless the player guesses the word before
-    
-    letter = input("guess " + str(k + 1) + " out of 9. enter a letter: ")
-    if letter in container:
-        print("Right guess!!")
-        cur_word = updated_current_word(container, cur_word, letter)
-        print(cur_word)
-
+while(k != 9 ): # 9 parts of the body which need to be drawn unless the player guesses the word before
+    if '*' in cur_word:
+        letter = input(str(9 - k) + " guesses left! enter a letter: ")[:1]
+        if letter in container:
+                if letter in cur_word:
+                    print("You already guessed that letter. try again")
+                    continue
+                print("Right guess!!")
+                cur_word = updated_current_word(container, cur_word, letter)
+                print("".join(cur_word))     
+        else:
+            print("Wrong guess!")
+            k += 1
+    else:
+            print("You guessed the word!\n Felicitas!")
+            break
